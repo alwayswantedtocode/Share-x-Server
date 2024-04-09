5df6aa87551e9aad997149c45628f1e6eedc7fa2
alwayswantedtocode <101648314+alwayswantedtocode@users.noreply.github.com>
2024-04-09 05:48:06 +0100

    commit update

--- Server/Config/AllowOrigins.js
+++ Server/Config/AllowOrigins.js
@@ -4,7 +4,7 @@ const AllowOrigins = [
   "http://localhost:3001",
   "http://localhost:3002",
   "http://localhost:5050",
-  //   "https://share-x-server.onrender.com",
+  "https://share-x-server.onrender.com",
 ];
 
 module.exports = AllowOrigins;
